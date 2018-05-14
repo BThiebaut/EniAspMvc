6c903cfe9ca28f2d4ed0addaef549e79bd0339e6
THIEBAUT Benjamin <benjamin.thiebaut2016@campus-eni.fr>
2018-05-14 10:26:04 +0200

    initial commit

---
+++ EniProjetMvc/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(EniProjetMvc.Startup))]
+namespace EniProjetMvc
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
