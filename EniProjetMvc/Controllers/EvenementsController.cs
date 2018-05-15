5db2de5f6f57bd4c3706e9fa8a53f45e1dd69a16
LAMBERT Tanneguy <tanneguy.lambert2016@campus-eni.fr>
2018-05-15 12:16:49 +0200

    début front liste evenement admin

--- EniProjetMvc/Controllers/EvenementsController.cs
+++ EniProjetMvc/Controllers/EvenementsController.cs
@@ -18,7 +18,99 @@ namespace EniProjetMvc.Controllers
         // GET: Evenements
         public ActionResult Index()
         {
-            return View(db.Evenements.ToList());
+
+           
+
+            Evenement e1 = new Evenement {
+                Id = 1,
+                DateDebut = new DateTime(),
+                DateFin = new DateTime(),
+                HeureFermeture = new DateTime(),
+                HeureOuverture = new DateTime(),
+                Intitule = "L'attaque des martiens",
+                Adresse = "le Monde",
+                Statut = StatutEvenement.EN_COUR
+            };
+
+            List<Image> images1 = new List<Image> {
+                new Image { Id = 1, Evenement = e1, Url = "http://planete-cine.com/wp-content/uploads/2016/04/mars-attacks.jpg"},
+            };
+            e1.Images = images1;
+
+            Evenement e2 = new Evenement
+            {
+                Id = 2,
+                DateDebut = new DateTime(),
+                DateFin = new DateTime(),
+                HeureFermeture = new DateTime(),
+                HeureOuverture = new DateTime(),
+                Intitule = "La sortie de Half-Life 3",
+                Adresse = "???",
+                Statut = StatutEvenement.A_VENIR
+            };
+
+            List<Image> images2 = new List<Image> {
+                new Image { Id = 2, Evenement = e2, Url = "http://static.papergeek.fr/2016/08/half-life-3-logo-720x450.jpg" },
+                new Image {Id = 3, Evenement = e2, Url = "https://cdn.drawception.com/images/panels/2014/8-3/8AXwxdYLEn-4.png" }
+            };
+            e2.Images = images2;
+
+            Evenement e3 = new Evenement
+            {
+                Id = 3,
+                DateDebut = new DateTime(),
+                DateFin = new DateTime(),
+                HeureFermeture = new DateTime(),
+                HeureOuverture = new DateTime(),
+                Intitule = "Phillipe !",
+                Adresse = "la ou il se cache",
+                Statut = StatutEvenement.EN_COUR
+            };
+
+            List<Image> images3 = new List<Image> {
+                new Image { Id = 4, Evenement = e1, Url = "https://i.ytimg.com/vi/EOxUWLl2HFs/hqdefault.jpg"},
+            };
+            e3.Images = images3;
+
+            Evenement e4 = new Evenement
+            {
+                Id = 4,
+                DateDebut = new DateTime(),
+                DateFin = new DateTime(),
+                HeureFermeture = new DateTime(),
+                HeureOuverture = new DateTime(),
+                Intitule = "La mort de Kenny",
+                Adresse = "Le parc du sud",
+                Statut = StatutEvenement.A_VENIR
+            };
+
+            List<Image> images4 = new List<Image> {
+                new Image { Id = 5, Evenement = e1, Url = "https://vignette.wikia.nocookie.net/southpark/images/4/42/Proper_condom_use_kenny%27s_death.jpg/revision/latest?cb=20100327195536"},
+            };
+            e4.Images = images4;
+
+            Evenement e5 = new Evenement
+            {
+                Id = 5,
+                DateDebut = new DateTime(),
+                DateFin = new DateTime(),
+                HeureFermeture = new DateTime(),
+                HeureOuverture = new DateTime(),
+                Intitule = "Une journée normale d'un joueur de League of légend",
+                Adresse = "n'importe ou",
+                Statut = StatutEvenement.A_VENIR
+            };
+
+            List<Image> images5 = new List<Image> {
+                new Image { Id = 6, Evenement = e1, Url = "http://www2.mes-coloriages-preferes.biz/colorino/Images/Large/Personnages-celebres-Troll-face-Rage-Guy-134663.png"},
+            };
+            e5.Images = images5;
+
+
+
+            List<Evenement> list = new List<Evenement> { e1, e2, e3, e4, e5};
+
+            return View(list);
         }
 
         // GET: Evenements/Details/5
